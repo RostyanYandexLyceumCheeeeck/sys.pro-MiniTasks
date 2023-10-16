313ecaec4d49117df3bf0b4331d5750cd5322d6a
Rostislav <zagigtovr97@gmail.com>
2023-10-16 10:24:59 +0500

    в 1 проход реализовал  ༼ つ ◕_◕ ༽つ

--- Mini tasks/task 04.py
+++ Mini tasks/task 04.py
@@ -1,18 +1,19 @@
-first = {'one_key': 22332,
-         'two_key': (1, 'a', 33.0),
-         111: 'asd',
-         'qqqqq': 22332}
+first = {
+    ('SSS+', '1000 - 7'): 22332,
+    'one_key': 22332,
+    'two_key': (1, 'a', 33.0),
+    111: 'asd',
+    'qqqqq': 22332,
+    'aass': 22332,
+    ('123', 'zxc'): 22332,
+    ('qwe', '222'): 22332,
+    }
 second: dict = {}
 
 for key, value in first.items():
     try:
-        second[value].append(key)
+        second[value] = (*second[value], key) if isinstance(second[value], tuple) else (second[value], key)
     except KeyError:
-        second[value] = key
-    except AttributeError:
-        second[value] = [second[value], key]
-
-for key, value in second.items():
-    second[key] = (*second[key], ) if isinstance(second[key], list) else second[key]
+        second[value] = (key, ) if isinstance(key, tuple) else key
 
 print(second)
