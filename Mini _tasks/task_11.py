dbcce152da28db887b237c05f648bc9cb6692002
Rostislav <zagigtovr97@gmail.com>
2023-12-24 00:05:26 +0500

    11 mini task new version

--- Mini _tasks/task_11.py
+++ Mini _tasks/task_11.py
@@ -1,20 +1,23 @@
 import sys
 
 
-def singleton(cls):
+def singleton(mcls):
     slov = {}
     count_ref = {}
 
-    def posrednik(*args, **kwargs):
-        if cls not in slov or sys.getrefcount(slov[cls]) == count_ref[cls]:
-            slov[cls] = cls(*args, **kwargs)
-            count_ref[cls] = sys.getrefcount(slov[cls])
-        return slov[cls]
-    return posrednik
+    class Intermediary(mcls):
+        def __new__(cls, *args, **kwargs):
+            if mcls not in slov or sys.getrefcount(slov[mcls]) == count_ref[mcls]:
+                slov[mcls] = mcls(*args, **kwargs)
+                count_ref[mcls] = sys.getrefcount(slov[mcls])
+            return slov[mcls]
+    return Intermediary
 
 
 @singleton
 class Foo:
+    test = 123
+
     def __init__(self):
         print('new Foo')
 
@@ -29,9 +32,10 @@ if __name__ == "__main__":
     # Test 1
     print("======Test 1======")
     a = Foo()
+    print(Foo.test)
     a1 = Foo()
     a2 = Foo()
-    print(id(a) == id(a1) == id(a2))
+    print(id(a) == id(a1) == id(a2))  # True
 
     # Test 2
     print("\n======Test 2======")
@@ -39,14 +43,14 @@ if __name__ == "__main__":
     save_id_b = id(b)
     del b
     b = Bar()
-    print(save_id_b == id(b))
+    print(save_id_b == id(b))  # False
 
     # Test 3
     print("\n======Test 3======")
     del a
     del a1
     a = Foo()
-    print(id(a) == id(a2))
+    print(id(a) == id(a2))  # True
 
     # Test 4
     print("\n======Test 4======")
@@ -54,4 +58,4 @@ if __name__ == "__main__":
     del a
     del a2
     a = Foo()
-    print(save_id_a == id(a))
+    print(save_id_a == id(a))  # False
