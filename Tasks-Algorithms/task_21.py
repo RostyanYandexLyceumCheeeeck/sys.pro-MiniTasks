ff32e2a6da49470dff200f5d4589d90c90d5c18c
Rostislav <zagigtovr97@gmail.com>
2024-03-22 22:29:10 +0500

    21 Algo. add tests

--- Tasks-Algorithms/task_21.py
+++ Tasks-Algorithms/task_21.py
@@ -1,17 +1,19 @@
 from math import inf
+from copy import copy
+from random import randint
 
 
 class BinomialTree:
-    def __init__(self, value=None, father=None, baby=None, neighbour=None, degree: int = 0):
+    def __init__(self, value=None, father=None, baby_right=None, baby_left=None, neighbour=None, degree: int = 0):
         self.value = value
         self.degree = degree
         self.father = father
-        self.baby_left = baby
-        self.baby_right = baby
+        self.baby_left = baby_left
+        self.baby_right = baby_right
         self.neighbour_left = neighbour
 
     def bind_baby(self, other):
-        if self.degree < 1:
+        if self.degree < 2:
             return
 
         old = self.baby_left.baby_left
@@ -19,6 +21,7 @@ class BinomialTree:
         while old:
             old.neighbour_left = new
             old = old.baby_left
+            new = new.neighbour_left
             new = new.baby_right
 
     def merge(self, other):
@@ -35,7 +38,7 @@ class BinomialTree:
         other.father = self
         self.degree += 1
 
-        if not other.degree:
+        if not other.degree:  # if self.degree == 1
             self.baby_right = other
         return self
 
@@ -53,11 +56,12 @@ class BinomialTree:
 class BinomialHeap:
     def __init__(self, roots=None):
         self.roots = roots
+        self.before_min = None
         self.min = None
 
     def insert(self, value):
         other = BinomialTree(value)
-        self.merge(BinomialHeap(other))
+        self.roots = self.merge(BinomialHeap(other)).roots
 
     def peek_min(self):
         if self.min:
@@ -65,15 +69,26 @@ class BinomialHeap:
 
         head = self.roots
         self.min = head
+        self.before_min = temp = None
         while head:
             if head.value < self.min.value:
                 self.min = head
+                self.before_min = temp
             head = head.neighbour_left
+            temp = head
         return self.min.value
 
     def extract_min(self):
         res = self.peek_min()
-        self.merge(res.baby_right)
+
+        # delete minimum
+        if self.before_min:
+            self.before_min.neighbour_left = self.min.neighbour_left
+        else:
+            self.roots = self.min.neighbour_left
+
+        self.roots = self.merge(BinomialHeap(self.min.baby_right)).roots
+        self.before_min = self.min = None
         return res
 
     def decrease_key(self, link: BinomialTree, key):
@@ -82,131 +97,194 @@ class BinomialHeap:
         link.sort_to_down()
 
     def merge(self, other):
-        other.roots.father = None
-        previous_head_res = self.roots
-        previous_head_other = other.roots
-        head_res = self.roots
+        if not other or not other.roots:
+            return self
+
+        if not self or not self.roots:
+            return other
+
+        result = BinomialHeap()
+        result.roots = BinomialTree()
+        result.min = self.min
+        if other.min and self.min and other.min < self.min:
+            result.min = other.min
+
+        head_res = result.roots
+        head_self = self.roots
         head_other = other.roots
-        carry = None
+        carry: BinomialTree | None = None
 
-        while head_res or head_other or carry:
-            if head_other:
-                head_other.father = None
+        while head_self or head_other:
+            head_res.father = None
 
-            if head_res is None:
-                if carry is None:  # only head_other
-                    previous_head_other.neighbour_left = None
-                    previous_head_res.neighbour_left = head_other
+            if not head_self:
+                if not carry:
+                    head_res.neighbour_left = head_other
                     break
-                if head_other is None:  # only carry
-                    previous_head_res.neighbour_left = carry
-                    break
-                if carry.degree < head_other.degree:
+                elif carry.degree < head_other.degree:
                     carry.neighbour_left = head_other
-                    previous_head_res.neighbour_left = carry
+                    head_res.neighbour_left = carry
+                    carry = None
                     break
+
                 carry = carry.merge(head_other)
-                carry.neighbour_left = None
-                previous_head_other = head_other
                 head_other = head_other.neighbour_left
-                continue
-
-            if head_other is None:
-                if carry is None:  # only head_res
-                    break
-                if carry.degree < head_res.degree:
-                    carry.neighbour_left = head_res
-                    previous_head_res.neighbour_left = carry
-                    break
-                carry = carry.merge(head_res)
                 carry.neighbour_left = None
-                previous_head_res = head_res
-                head_res = head_res.neighbour_left
                 continue
 
-            if carry is None:
-                if head_res.degree < head_other.degree:
-                    previous_head_res = head_res
-                    head_res = head_res.neighbour_left
-                    continue
+            elif not head_other:
+                if not carry:
+                    head_res.neighbour_left = head_self
+                    break
 
-                if head_res.degree == head_other.degree:
-                    carry = head_res.merge(head_other)
+                elif carry.degree < head_self.degree:
+                    carry.neighbour_left = head_self
+                    head_res.neighbour_left = carry
+                    carry = None
+                    break
+
+                carry = carry.merge(head_self)
+                head_self = head_self.neighbour_left
+                carry.neighbour_left = None
+                continue
+
+            elif not carry:
+                if head_self.degree < head_other.degree:
+                    head_res.neighbour_left = head_self
+                    head_self = head_self.neighbour_left
+                    head_res = head_res.neighbour_left
+
+                elif head_self.degree == head_other.degree:
+                    carry = head_self.merge(head_other)
+                    head_self = head_self.neighbour_left
+                    head_other = head_other.neighbour_left
                     carry.neighbour_left = None
-                    previous_head_res.neighbour_left = head_res.neighbour_left
+
+                else:
+                    head_res.neighbour_left = head_other
+                    head_other = head_other.neighbour_left
                     head_res = head_res.neighbour_left
-
-                elif head_res.degree > head_other.degree:
-                    previous_head_res.neighbour_left = head_other
-                    previous_head_res = head_other
-
-                previous_head_other = head_other
-                head_other = head_other.neighbour_left
                 continue
 
-            # head_res and head_other and carry
-            if head_res.degree > carry.degree < head_other.degree:
-                carry.neighbour_left = head_other if head_other.degree < head_res.degree else head_res
-                previous_head_res.neighbour_left = carry
-                previous_head_res = carry
+            if head_self.degree == head_other.degree == carry.degree:
+                head_res.neighbour_left = head_self  # add head_self
+                head_res = head_self
+                head_self = head_self.neighbour_left
+                # head_res.neighbour_left = None
+
+                carry = carry.merge(head_other)
+                head_other = head_other.neighbour_left
+                carry.neighbour_left = None
+
+            elif head_self.degree > carry.degree < head_other.degree:
+                head_res.neighbour_left = carry
+                head_res = carry
                 carry = None
 
-            elif head_res.degree == carry.degree == head_other.degree:
-                carry = carry.merge(head_other)
+            elif head_self.degree == carry.degree:
+                carry = carry.merge(head_self)
+                head_self = head_self.neighbour_left
                 carry.neighbour_left = None
-                previous_head_res = head_res
-                previous_head_other = head_other
-                head_res = head_res.neighbour_left
-                head_other = head_other.neighbour_left
-                # continue
 
-            elif head_res.degree == carry.degree:
-                carry = carry.merge(head_res)
-                carry.neighbour_left = None
-                previous_head_res.neighbour_left = head_res.neighbour_left
-                previous_head_res = head_res
-                head_res = head_res.neighbour_left
-
-            else:  # head_res.degree > carry.degree == head_other_degree
+            else:  # head_self.degree > head_other.degree == carry.degree
                 carry = carry.merge(head_other)
-                carry.neighbour_left = head_res
-                previous_head_res.neighbour_left = carry
-                previous_head_res = carry
-                previous_head_other = head_other
                 head_other = head_other.neighbour_left
+                carry.neighbour_left = None
 
-        return self.roots
+        if carry:
+            carry.father = None
+            head_res.neighbour_left = carry
+
+        result.roots = result.roots.neighbour_left
+        return result
 
     def delete(self, link):
         self.decrease_key(link, -inf)
         self.extract_min()
 
+    def heap_to_number(self):
+        res = 0
+        head = self.roots
+        while head:
+            res += 2 ** head.degree
+            head = head.neighbour_left
+
+        return res
+
+
+def createBinomialTree(size, arr: list | None = None, shift: int = 0) -> BinomialTree:
+    if not size:
+        return BinomialTree()
+
+    if not arr:
+        arr = [shift + x for x in range(size)]
+
+    size = len(arr)
+
+    arr = copy(arr)
+    for i in range(len(arr)):
+        arr[i] = BinomialTree(arr[i])
+
+    step = 1
+    while step < size:
+        for i in range(0, size - step, 2 * step):
+            arr[i] = arr[i].merge(arr[i + step])
+        step *= 2
+    return arr[i]
+
+
+def createBinomialHeap(pattern: int | str = '0', shift: int = 0) -> BinomialHeap:
+    if isinstance(pattern, int):
+        pattern = bin(pattern)[2:]
+
+    res = BinomialHeap()
+    size = 1
+    for i in range(-1, -len(pattern) - 1, -1):
+        bit = int(pattern[i])
+        if bit:
+            temp = createBinomialTree(size, shift=size - 1 + shift)
+            res = res.merge(BinomialHeap(temp))
+        size *= 2
+    return res
+
+
+def testing_insert(heap: BinomialHeap, value):
+    before = heap.heap_to_number()
+    heap.insert(value)
+    after = heap.heap_to_number()
+    assert after == before + 1
+
+
+def testing_extract_min(heap: BinomialHeap):
+    before = heap.heap_to_number()
+    res = heap.extract_min()
+    print('ext_min = ', res)
+    after = heap.heap_to_number()
+    assert after == before - 1
+
+
+def testing_decrease_key(heap: BinomialHeap, elem, key):
+    heap.decrease_key(elem, key)
+    t = 3   # не знаю, как ассёртом проверить. Тестил дебаггером в пайчарме >_<
+
+
+def testing_delete(heap: BinomialHeap, elem):
+    before = heap.heap_to_number()
+    heap.delete(elem)
+    after = heap.heap_to_number()
+    assert after == before - 1
+
 
 if __name__ == "__main__":
-    one = BinomialTree(1)
-    two = BinomialTree(2)
-    three = BinomialTree(3)
-    four = BinomialTree(4)
-    five = BinomialTree(5)
-    six = BinomialTree(6)
+    one = createBinomialTree(8)
 
-    one = one.merge(two)
+    qwe = createBinomialHeap('1101')
 
-    three = three.merge(four)
-    six = five.merge(six)
-    three = three.merge(six)
+    asd = createBinomialHeap('1111', 15)
 
-    one.neighbour_left = three
-    asd = BinomialHeap(one)
+    zxc = asd.merge(qwe)  # '11100'
 
-    seven = BinomialTree(7)
-
-    eight = BinomialTree(8)
-    nine = BinomialTree(9)
-    eight = eight.merge(nine)
-
-    seven.neighbour_left = eight
-
-    qwe = BinomialHeap(seven)
-
-    qwe.merge(asd)
+    testing_insert(zxc, 0)
+    testing_extract_min(zxc)
+    testing_decrease_key(zxc, zxc.roots.neighbour_left.baby_left, -1)
+    testing_delete(zxc, zxc.roots)
