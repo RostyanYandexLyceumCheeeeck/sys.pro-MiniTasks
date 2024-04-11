d877e9433904dd13617c8ed570b81113fad19256
Rostislav <zagigtovr97@gmail.com>
2024-04-12 00:42:13 +0500

    made the comments look nice

--- Tasks-Algorithms/task_26.py
+++ Tasks-Algorithms/task_26.py
@@ -40,9 +40,9 @@ class Solution:
 
 
 def test1():  # [1,null,2,null,3,null,4]
-    root = TreeNode(1)                    # . 1══╗                            ╔══3══╗
-    root.right = TreeNode(2)              # . |  2══╗       ========\      ╔══2  |  4
-    root.right.right = TreeNode(3)        # . |  |  3══╗    ========/      1  |  |
+    root = TreeNode(1)                    # . 1══╗                                           ╔══3══╗
+    root.right = TreeNode(2)              # . |  2══╗                      ========\      ╔══2  |  4
+    root.right.right = TreeNode(3)        # . |  |  3══╗                   ========/      1  |  |
     root.right.right.right = TreeNode(4)  # . |  |  |  4
 
     res = Solution()
@@ -51,11 +51,11 @@ def test1():  # [1,null,2,null,3,null,4]
 
 
 def test2():  # [19,10,null,4,17,null,5]
-    root = TreeNode(19)                 # .              ╔═══════19                ╔══10═════╗
-    root.left = TreeNode(10)            # .         ╔═══10═══╗    |  ========\   ╔═5   | ╔══19
-    root.left.left = TreeNode(4)        # .         4═╗  |   17   |  ========/   4 |   | 17  |
-    root.left.right = TreeNode(17)      # .         | 5  |        |
-    root.left.left.right = TreeNode(5)  #
+    root = TreeNode(19)                   # .     ╔═══════19                             ╔══10═════╗
+    root.left = TreeNode(10)              # . ╔═══10═══╗   |               ========\   ╔═5   | ╔══19
+    root.left.left = TreeNode(4)          # . 4═╗  |   17  |               ========/   4 |   | 17  |
+    root.left.right = TreeNode(17)        # . | 5  |       |
+    root.left.left.right = TreeNode(5)    # .
 
     res = Solution()
     qwe = res.balanceBST(root)
@@ -63,13 +63,13 @@ def test2():  # [19,10,null,4,17,null,5]
 
 
 def test3():  # [1,null,15,14,17,7,null,null,null,2,12,null,3,9,null,null,null,null,11]
-    root = TreeNode(1)              # 1═══════════════════╗
-    l2 = root.right = TreeNode(15)  # |              ╔════15═══╗                    ╔════11════════╗
-    l2.right = TreeNode(17)         # |      ╔══════14    |   17   ========\     ╔══7══╗ |     ╔══15══╗
-    l3 = l2.left = TreeNode(14)     # | ╔════7════╗  |    |        ========/   ╔═2  |  9 |  ╔═14   | 17
-    l4 = l3.left = TreeNode(7)      # | 2═══╗|╔═══12 |    |                    1 |  |    |  12 |   |
-    l4.left = TreeNode(2)           # | |   3|9═╗  | |    |
-    l5 = l4.right = TreeNode(12)    # | |    || 11 | |    |
+    root = TreeNode(1)                    # . 1═══════════════════╗
+    l2 = root.right = TreeNode(15)        # . |              ╔════15═══╗                    ╔════11════════╗
+    l2.right = TreeNode(17)               # . |      ╔══════14    |   17   ========\     ╔══7══╗ |     ╔══15══╗
+    l3 = l2.left = TreeNode(14)           # . | ╔════7════╗  |    |        ========/   ╔═2  |  9 |  ╔═14   | 17
+    l4 = l3.left = TreeNode(7)            # . | 2═══╗|╔═══12 |    |                    1 |  |    |  12 |   |
+    l4.left = TreeNode(2)                 # . | |   3|9═╗  | |    |
+    l5 = l4.right = TreeNode(12)          # . | |    || 11 | |    |
     l5.left = TreeNode(9)
     l5.left.right = TreeNode(11)
 
