45419a2acb7a2301e53eb3895305accea19ee630
Rostislav <zagigtovr97@gmail.com>
2024-09-28 05:37:58 +0700

    update 36 mini-task(add new tests and delete kostil`)

--- Tasks-Algorithms/task_36.py
+++ Tasks-Algorithms/task_36.py
@@ -17,11 +17,15 @@ class UnionFind:
 
         if self.rank[one_parent] < self.rank[two_parent]:
             self.parent[one_parent] = two_parent
-            self.count[two_parent] += self.count[one_parent]
+
+            t = (two_parent - self.count[two_parent]) % len(self.count)
+            self.count[two_parent] += self.count[one_parent] * ((one_parent < two_parent) or (t == one_parent))
         else:
             self.parent[two_parent] = one_parent
-            self.count[one_parent] += self.count[two_parent] * (one_parent != two_parent)
             self.rank[one_parent] += (self.rank[one_parent] == self.rank[two_parent])
+
+            t = (one_parent - self.count[one_parent]) % len(self.count)
+            self.count[one_parent] += self.count[two_parent] * ((two_parent < one_parent) or (t == two_parent))
 
     def get_free(self, elem):
         parent = self.find(elem)
@@ -42,7 +46,7 @@ def scheduling(tasks: list[tuple[int, int]]):
     search = UnionFind(size)
     schedule = [(0, 0)] * size
 
-    for i in range(len(tasks) - 1):
+    for i in range(len(tasks)):
         deadline, fine = tasks[i]
 
         parent = search.find(deadline)
@@ -50,11 +54,6 @@ def scheduling(tasks: list[tuple[int, int]]):
 
         schedule[free] = tasks[i]
         search.union(free, parent)
-
-    for i in range(-1, -len(tasks) - 1, -1):
-        if schedule[i] == (0, 0):
-            schedule[i] = tasks[-1]
-            break
 
     result_fine = 0
     for day in range(len(schedule)):
@@ -65,39 +64,64 @@ def scheduling(tasks: list[tuple[int, int]]):
     return result_fine
 
 
-def test0():
+def test00():
     arr = [(2, 25), (0, 30), (2, 50), (3, 10), (2, 15)]
     assert scheduling(arr) == 15
 
 
-def test1():
+def test01():
     arr = [(0, 42), (1, 69), (2, 420), (3, 1997), (3, 23126), (4, 2005), (5, 0)]
     assert scheduling(arr) == 42
 
 
-def test2():
+def test02():
     arr = []
     assert scheduling(arr) == 0
 
 
-def test3():
+def test03():
     arr = [(11, 1), (11, 1), (11, 1), (11, 1), (11, 1), (11, 1), (11, 1), (11, 1), (11, 1), (11, 1), (11, 1), (11, 1)]
     assert scheduling(arr) == 0
 
 
-def test4():
+def test04():
     arr = [(11, 1), (1, 11), (1, 11), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1)]
     assert scheduling(arr) == 9
 
 
-def test5():
+def test05():
     arr = [(0, 12), (1, 12), (2, 12), (3, 12), (4, 12), (5, 12), (6, 12), (8, 11), (8, 12), (8, 12), (9, 12), (9, 1)]
     assert scheduling(arr) == 12
 
 
-def test6():
+def test06():
     arr = [(0, 6), (1, 7), (2, 8), (3, 9), (4, 10), (5, 11), (6, 12), (8, 11), (8, 12), (8, 13), (9, 12), (9, 1)]
     assert scheduling(arr) == 7
+
+
+def test07():
+    arr = [(0, 12), (1, 12), (2, 12), (3, 12), (4, 12), (5, 12), (6, 12), (6, 11), (6, 10), (6, 9), (6, 8), (11, 1)]
+    assert scheduling(arr) == 38
+
+
+def test08():
+    arr = [(0, 8), (0, 38)]
+    assert scheduling(arr) == 8
+
+
+def test09():
+    arr = [(0, 8), (0, 0)]
+    assert scheduling(arr) == 0
+
+
+def test10():
+    arr = [(0, 8), (0, 1), (0, 1), (0, 1), (0, 7)]
+    assert scheduling(arr) == 10
+
+
+def test11():
+    arr = [(3, 30), (2, 20), (1, 10), (0, 15), (2, 25)]
+    assert scheduling(arr) == 10
 
 
 def check_time_test(func):
@@ -108,13 +132,21 @@ def check_time_test(func):
     print()
 
 
+def all_tests():
+    check_time_test(test00)
+    check_time_test(test01)
+    check_time_test(test02)
+    check_time_test(test03)
+    check_time_test(test04)
+    check_time_test(test05)
+    check_time_test(test06)
+    check_time_test(test07)
+    check_time_test(test08)
+    check_time_test(test09)
+    check_time_test(test10)
+    check_time_test(test11)
+
+
 if __name__ == "__main__":
-    check_time_test(test0)
-    check_time_test(test1)
-    check_time_test(test2)
-    check_time_test(test3)
-    check_time_test(test4)
-    check_time_test(test5)
-    check_time_test(test6)
-
-
+    all_tests()
+    
